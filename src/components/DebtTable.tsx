eb6feb9c072f5208366e281f600f56e070a9a56c
Cameron Brouk <cameronbrouk@gmail.com>
2021-06-17 12:31:32 -0500

    add base components for debt table and row

---
+++ src/components/DebtTable.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const DebtTable = () => {
+  return (
+    <table>
+      <th></th>
+      <tbody></tbody>
+    </table>
+  )
+}
---
+++ src/components/DebtTableRow.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+  debtInfo: object
+}
+
+export const FinancialDataTableRow = (props: Props) => {
+  return (
+    <tr>
+      <td></td>
+    </tr>
+  )
+}
